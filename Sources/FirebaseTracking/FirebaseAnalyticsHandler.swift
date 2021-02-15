cb4b8aa02cc9b806521d0a06d660a752933d1eac
kkla320 <accounts_kkla@icloud.com>
2021-02-15 16:23:31 +0100

    - Converting metadata to NSObject

--- Sources/FirebaseTracking/FirebaseAnalyticsHandler.swift
+++ Sources/FirebaseTracking/FirebaseAnalyticsHandler.swift
@@ -18,13 +18,35 @@ public class FirebaseAnalyticsHandler: AnalyticsHandler {
 
 extension Optional where Wrapped == Tracking.Analytics.Metadata {
     func toFirebaseEventMetadata() -> [String: Any] {
-        var parameters: [String: Any] = self ?? [:]
+        var parameters: [String: Tracking.Analytics.MetadataValue] = self ?? [:]
         if let screenName = parameters.removeValue(forKey: "screen-name") {
             parameters[FirebaseAnalytics.AnalyticsParameterScreenName] = screenName
         }
         if let screenClass = parameters.removeValue(forKey: "screen-sender") {
             parameters[FirebaseAnalytics.AnalyticsParameterScreenClass] = screenClass
         }
-        return parameters
+        
+        return parameters.mapValues({ $0.toNSObject() })
+    }
+}
+
+extension Tracking.Analytics.MetadataValue {
+    func toNSObject() -> NSObject {
+        switch self {
+        case .string(let string):
+            return NSString(string: string)
+        case .nil:
+            return NSNull()
+        case .float(let float):
+            return NSNumber(value: float)
+        case .int(let int):
+            return NSNumber(value: int)
+        case .bool(let bool):
+            return NSNumber(booleanLiteral: bool)
+        case .array(let array):
+            return NSArray(array: array.map{ $0.toNSObject() })
+        case .dictionary(let dictionary):
+            return NSDictionary(dictionary: dictionary)
+        }
     }
 }
