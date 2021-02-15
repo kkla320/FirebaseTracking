5285742b1e42c5d5c2512a4a7e2dee6454f993a8
kkla320 <accounts_kkla@icloud.com>
2021-02-15 10:09:11 +0100

    - Updated to newest Analytics

---
+++ Sources/FirebaseTracking/FirebaseAnalyticsHandler.swift
@@ -0,0 +1,26 @@
+import Tracking
+import FirebaseAnalytics
+
+public class FirebaseAnalyticsHandler: AnalyticsHandler {
+    public func logEvent(_ name: String, parameter: Tracking.Analytics.Metadata?) {
+        if name == ScreenEvent.name {
+            FirebaseAnalytics.Analytics.logEvent(FirebaseAnalytics.AnalyticsEventScreenView, parameters: parameter.toFirebaseEventMetadata())
+        }
+        else {
+            FirebaseAnalytics.Analytics.logEvent(name, parameters: parameter)
+        }
+    }
+}
+
+extension Optional where Wrapped == Tracking.Analytics.Metadata {
+    func toFirebaseEventMetadata() -> [String: Any] {
+        var parameters: [String: Any] = self ?? [:]
+        if let screenName = parameters.removeValue(forKey: "screen-name") {
+            parameters[FirebaseAnalytics.AnalyticsParameterScreenName] = screenName
+        }
+        if let screenClass = parameters.removeValue(forKey: "screen-sender") {
+            parameters[FirebaseAnalytics.AnalyticsParameterScreenClass] = screenClass
+        }
+        return parameters
+    }
+}
--- Sources/FirebaseTracking/FirebaseTracking.swift
+++
@@ -1,8 +0,0 @@
-import Tracking
-import FirebaseAnalytics
-
-public class FirebaseAnalyticsHandler: AnalyticsHandler {
-    public func logEvent(_ name: String, parameter: Tracking.Analytics.Metadata?) {
-        FirebaseAnalytics.Analytics.logEvent(name, parameters: parameter)
-    }
-}
